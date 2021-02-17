f6951237a52a05b4e586928e2bbcf579398bcc4a
Holger Lersch <holger@lersch.net>
2021-02-17 07:38:59 -0500

    no major changes, only to keep the code base consitent

--- models/card.js
+++ models/card.js
@@ -1,10 +1,12 @@
 class Card {
     constructor(id, suit, rank, myRank, open) {
       this.id = id;
-      this.suit = suit;
-      this.rank = rank;
-      this.myRank = myRank;
-      this.open = open;
+      this.suit = suit;     
+      this.rank = rank;     //- common rank of the card
+      
+      this.myRank = myRank; //- rank of the card for the concrete game like e.g. Tuppen
+      
+      this.open = open;     //- card is open to all players (is played)
     }
     isPlayed() {
       this.open = true;
